a31af215f5b5365974d3ace3898cc3dafe00db2c
Esraa Abduallah <esraahisham753@gmail.com>
2024-03-18 11:14:25 +0000

    Complete toggle mode script

--- app/js/script.js
+++ app/js/script.js
@@ -6,6 +6,58 @@ const lightRadio = document.getElementById("light");
 
 const body = document.querySelector("body");
 
+let darkMode = false;
+let userModeSet = false;
+
+const loadMode = () => {
+  const esdDarkMode = JSON.parse(localStorage.getItem("esdDarkMode"));
+
+  if (esdDarkMode === null) {
+    darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    userModeSet = false;
+  } else {
+    userModeSet = true;
+    darkMode = esdDarkMode;
+  }
+};
+
+const changeMode = () => {
+  body.classList = darkMode ? "dark" : "light";
+  darkRadio.checked = darkMode;
+  lightRadio.checked = !darkMode;
+};
+
+loadMode();
+changeMode();
+
+const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark");
+const lightModeQuery = window.matchMedia("(prefers-color-scheme: light)");
+
+darkModeQuery.addEventListener("change", (event) => {
+  if (!userModeSet) {
+    if (event.matches) {
+      darkMode = true;
+    } else {
+      darkMode = false;
+    }
+    changeMode();
+  }
+});
+
+lightModeQuery.addEventListener("change", (event) => {
+  if (!userModeSet) {
+    if (!event.matches) {
+      darkMode = true;
+    } else {
+      darkMode = false;
+    }
+    changeMode();
+  }
+});
+
 toggleEl.addEventListener("click", function () {
-  body.classList = darkRadio.checked ? "dark" : "light";
+  darkMode = darkRadio.checked ? true : false;
+  changeMode();
+  localStorage.setItem("esdDarkMode", JSON.stringify(darkMode));
+  userModeSet = true;
 });
